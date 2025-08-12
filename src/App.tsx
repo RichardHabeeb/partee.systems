e51bcec9cd49c43e8fb65874b6ad8198982b72b8
Richard Habeeb <richard.habeeb@gmail.com>
2025-08-12 16:22:24 -0400

    Font update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: Component = () => {
     <div class={styles.container}>
       <main class={styles.main}>
         <header class={styles.header}>
-          <h1 class={styles.title}>PARTEE</h1>
+          <h1 class={styles.title}>PARTEE!</h1>
           <p class={styles.subtitle}>
             The fast, usable, and secure enclave platform for latency-sensitive applications.
           </p>
